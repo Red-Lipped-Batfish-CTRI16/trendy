98b7e0e39bed2d782d28c11cd8864a850127aae9
Alan Beck <kalanbeck@Skunk>
2023-05-27 22:00:48 -0400

    got fetch and card display working

--- client/pages/Main.js
+++ client/pages/Main.js
@@ -1,34 +1,42 @@
 import Carousel from "../containers/Carousel";
 import AppCard from "../components/AppCard"
+import { useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 export default function Main() {
   let location = useLocation();
   const { responseData } = location.state;
-  
-  useEffect(() => {
-    const cards = responseData.map((card, index) => {
-      <div key={index} className='card'>
-        <h1>{card.name}</h1>
-        <p>{card.rating}</p>
-        <img url={card.image_url}/>
-        <p>{card.url}</p>
-        <p>{card.categories}</p>
-        <p>{card.location}</p>
-      </div>
-    })
-  }, [location]);
+  const [cards, setCards] = useState([]); // Use state to store the cards
 
-  //
+  useEffect(() => {
+    const cardElements = responseData.map((card, index) => (
+      <div key={index} className="card">
+        <h1>{card.name}</h1>
+        <p>Score: {Math.round(card.averageScore * 100)}</p>
+        <p>Category: {card.categories[0].title}</p>
+        <p>Address: {card.location[0]}</p>
+        <p>{truncateLink(card.url)}</p>
+        <img src={card.image_url}/>
+      </div>
+    ));
+
+    setCards(cardElements); // Update the state with the generated cards
+  }, [responseData]);
+
+  const truncateLink = (url) => {
+    const maxLength = 30; // Maximum length of the displayed link
+    if (url.length > maxLength) {
+      return url.substring(0, maxLength) + "...";
+    }
+    return url;
+  };
 
   return (
-    <div className="Main">
-      <h1>Trending *User Input* near *User Location*</h1>
-        <div className="main-image">
-
-        <img src="./image/map.png"></img>
-        </div>
-      <Carousel/>      
-    
+    <div>
+      <h1>Main</h1>
+      <div className="cardDisplay">
+        {cards}
+      </div>
     </div>
-  )
-} 
+  );
+}
