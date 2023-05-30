f1b142dc204a70d61960b4444350fbd5cc39db6c
peter <tranpeter1095@gmail.com>
2023-05-29 18:29:51 -0600

    jwt will add auth token upon correct username+pw at login

--- server/controllers/passwordController.js
+++ server/controllers/passwordController.js
@@ -13,4 +13,16 @@ passwordController.hashPassword = (req, res, next) => {
   });
 };
 
+passwordController.authenticateToken = (req, res, next) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    return next();
+  });
+};
+
 module.exports = passwordController;
