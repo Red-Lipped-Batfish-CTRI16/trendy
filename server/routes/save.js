ca52bd820ac07b7afb703b0a9f7746c4037c5e6f
staceyjhlee <staceyjlee18@gmail.com>
2023-05-31 11:56:15 -0400

    new-database

--- server/routes/save.js
+++ server/routes/save.js
@@ -1,19 +1,19 @@
-const express = require("express");
-const router = express.Router();
-import saveController from "../controllers/saveController";
+// const express = require("express");
+// const router = express.Router();
+// import saveController from "../controllers/saveController";
 
-router.get("/", saveController.getSaved, (req, res, next) => {
-  //sending back
-  console.log("now in saveController.getSaved");
-  res.status(200).json(res.locals.saved);
-});
+// router.get("/", saveController.getSaved, (req, res, next) => {
+//   //sending back
+//   console.log("now in saveController.getSaved");
+//   res.status(200).json(res.locals.saved);
+// });
 
-router.post("/", saveController.addSaved, (req, res, next) => {
-  //sending back list of selection with all properties? 
-  res.status(200).json("send all likes here");
-});
+// router.post("/", saveController.addSaved, (req, res, next) => {
+//   //sending back list of selection with all properties?
+//   res.status(200).json("send all likes here");
+// });
 
-//should this be DELETE or POST ?
-router.delete('/', saveController.removeSaved, (req, res, next) => {
-res.json('send updated list')
-})
+// //should this be DELETE or POST ?
+// router.delete('/', saveController.removeSaved, (req, res, next) => {
+// res.json('send updated list')
+// })
--- server/server.js
+++ server/server.js
@@ -1,31 +1,31 @@
-require('dotenv').config();
+require("dotenv").config();
 /* eslint no-unused-vars: 0 */
-const path = require('path');
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const signupRouter = require('./routes/signup');
-const loginRouter = require('./routes/login');
-const searchRouter = require('./routes/search');
-const userRouter = require('./routes/user');
-const businessRouter = require('./routes/business');
-const favRouter = require('./routes/fav');
+const path = require("path");
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const signupRouter = require("./routes/signup");
+const loginRouter = require("./routes/login");
+const searchRouter = require("./routes/search");
+const userRouter = require("./routes/user");
+const businessRouter = require("./routes/business");
+const favRouter = require("./routes/fav");
 const app = express();
 const port = 3000;
 
-app.use(express.static('public', { extensions: ['css'] }));
-app.use(express.static(path.resolve(__dirname, '../dist')));
+app.use(express.static("public", { extensions: ["css"] }));
+app.use(express.static(path.resolve(__dirname, "../dist")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('client'));
+app.use(express.static("client"));
 app.use(cookieParser());
 
-app.use('/api/search', searchRouter);
-app.use('/api/user', userRouter);
-app.use('/api/business', businessRouter);
-app.use('/api/fav', favRouter);
-app.use('/api/signup', signupRouter);
-app.use('/api/login', loginRouter);
-app.use('/api/save', saveRouter)
+app.use("/api/search", searchRouter);
+app.use("/api/user", userRouter);
+app.use("/api/business", businessRouter);
+app.use("/api/fav", favRouter);
+app.use("/api/signup", signupRouter);
+app.use("/api/login", loginRouter);
+// app.use('/api/save', saveRouter)
 
 app.use(({ code, error }, req, res, next) => {
   res.status(code).json({ error: error.message });
