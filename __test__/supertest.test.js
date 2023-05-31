a1d8c1f61e9d10ca766fe3a4296d57b34a869ba1
Stephen Lemlek <stephenlemlek@Stephens-MacBook-Pro.local>
2023-05-31 17:19:26 -0500

    supertest, 2 tests run

--- __test__/supertest.test.js
+++ __test__/supertest.test.js
@@ -1,1 +1,45 @@
-const request = require('supertest')
+const request = require('supertest');
+const assert = require('assert');
+const express = require('express');
+
+const app = express();
+
+const server = 'http://localhost:3000';
+
+
+
+
+describe('Route integration', () => {
+    describe('/', () => {
+        describe('GET', () => {
+            // Note that we return the evaluation of `request` here! It evaluates to
+            // a promise, so Jest knows not to say this test passes until that
+            // promise resolves. See https://jestjs.io/docs/en/asynchronous
+            it('responds with 200 status', () => request(server)
+                .get('/')
+                // .expect('Content-Type', /json/)
+                // .expect('Content-Length', '15')
+                .expect(200)
+                
+            )
+
+            it('responds with html', () => request(server)
+                .get('/')
+                .expect('Content-Type', /text\/html/)
+                // .expect('Content-Length', '15')
+                // .expect(200)
+                
+            )
+        })
+    })
+
+    describe('/api/search', () => {
+        const query = {interest: 'bar', radius: '5'}
+        it('responds with json', () => request(server)
+            .get('/api/search')
+            .expect('Content-Type', /application\/json/)
+        )
+    })
+})
+    
+
