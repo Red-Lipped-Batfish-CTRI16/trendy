ad7f4c94f807a4deaca2bc71e88cf74ede22d632
Stephen Lemlek <stephenlemlek@Stephens-MacBook-Pro.local>
2023-05-31 11:18:49 -0500

    updated with pseudcode

---
+++ __test__/db.js
@@ -0,0 +1,28 @@
+//------------ REQUIRES------------//
+
+const fs = require('fs')
+
+
+//------------ DB USER TESTS ------------//
+
+//------------ PW HASH TESTS ------------//
+
+//SUCCESSFULLY HASHES PASSWORD
+
+//------------ ADD USER TESTS ------------//
+
+//WRITES A VALID USER TO THE USER TABLE
+
+//RETURNS AN ERROR WHEN USERNAME ALREADY EXISTS
+
+
+
+//------------ LOGIN USER TESTS ------------//
+
+//SUCCESSFULLY LOGIN WITH VALID USERNAME AND PWORD
+
+//FAIL TO LOGIN WITH INVALID USERNAME AND PWORD
+
+
+
+
